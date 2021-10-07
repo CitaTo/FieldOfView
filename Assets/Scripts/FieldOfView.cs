e4538f6d9e0010140643eed6269a515987e8ef69
Thanh To Chi <thanh.to-chi@ubisoft.com>
2021-10-07 16:43:07 +0700

    add 2 fow lines to editor

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -1,12 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FieldOfView : MonoBehaviour {
+public class FieldOfView : MonoBehaviour
+{
 
     public float viewRadius;
+    [Range(0, 360)]
     public float viewAngle;
 
-    public Vector3 DirFromAngle(float angleInDegress){
+    public Vector3 DirFromAngle(float angleInDegress, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegress += transform.eulerAngles.y;
+        }
         Vector3 result = new Vector3(Mathf.Sin(angleInDegress * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegress * Mathf.Deg2Rad));
         return result;
     }
