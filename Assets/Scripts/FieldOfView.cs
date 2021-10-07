bd54c9dc277c74f691c4e6b91f368544f8464863
Thanh To Chi <thanh.to-chi@ubisoft.com>
2021-10-07 16:12:02 +0700

    add DirFromAngle function

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -3,4 +3,11 @@ using System.Collections;
 
 public class FieldOfView : MonoBehaviour {
 
+    public float viewRadius;
+    public float viewAngle;
+
+    public Vector3 DirFromAngle(float angleInDegress){
+        Vector3 result = new Vector3(Mathf.Sin(angleInDegress * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegress * Mathf.Deg2Rad));
+        return result;
+    }
 }
